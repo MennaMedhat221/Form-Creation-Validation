9d556ef7aa7bc035a346769b87105b598044f027
MennaMedhatt <menna.medhat@thefabulous.co>
2025-01-28 23:19:51 +0200

    fix

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('registration-form');
     const feedbackDiv = document.getElementById('form-feedback');
-    const username = document.getElementById('username');
-    const email = document.getElementById('email');
-    const password = document.getElementById('password');
+    const usernameElement = document.getElementById('username');
+    const emailElement = document.getElementById('email');
+    const passwordElement = document.getElementById('password');
+
+
     let isValid = true;
     let messages = [];
     form.addEventListener('submit', function(event) {
+        const username = usernameElement.value.trim();
+        const email = emailElement.value.trim();
+        const password = passwordElement.value.trim();
+
         event.preventDefault();
         feedbackDiv.innerHTML = '';
         messages=[];
 
-        if(username.value.trim().length < 3) {
+        if(username.length < 3) {
             isValid = false;
             messages.push('Username must be at least 3 characters long');
         }
-        if(!email.value.trim().includes('@') || !email.value.trim().includes('.')) {
+        if(!email.includes('@') || !email.includes('.')) {
             isValid = false;
             messages.push('Email must contain @ and .');
         }
-        if(password.value.trim().length < 8) {
+        if(password.length < 8) {
             isValid = false;
             messages.push('Password must be at least 8 characters long');
         }
